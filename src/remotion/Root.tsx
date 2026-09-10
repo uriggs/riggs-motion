93aef2ea502f5db25807852d1beb63eb8ef3e9c3
uriggs <31654526+uriggs@users.noreply.github.com>
2026-09-10 16:01:39 -0400

    Register deterministic Riggs Signals film composition

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Composition } from "remotion";
 import { DynamicComp } from "./DynamicComp";
+import { RiggsSignalsFilm } from "./RiggsSignalsFilm";
 
 const defaultCode = `import { AbsoluteFill } from "remotion";
 export const MyAnimation = () => <AbsoluteFill style={{ backgroundColor: "#000" }} />;`;
@@ -8,6 +9,14 @@ export const MyAnimation = () => <AbsoluteFill style={{ backgroundColor: "#000" 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      <Composition
+        id="RiggsSignalsFilm"
+        component={RiggsSignalsFilm}
+        durationInFrames={600}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
       <Composition
         id="DynamicComp"
         component={DynamicComp}
